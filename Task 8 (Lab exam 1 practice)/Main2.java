b3b65aa544b659f58e376c585896f5b9c54ccdcc
priomdhar <priomdhar1010@gmail.com>
2026-07-23 16:34:20 +0600

    Update Main2.java

--- Task 8 (Lab exam 1 practice)/Main2.java
+++ Task 8 (Lab exam 1 practice)/Main2.java
@@ -1,1 +1,77 @@
+import java.util.Scanner;
+class ShoppingCart {
+    String[] itemNames = new String[100];
+    double[] prices = new double[100];
+    int[] quantities = new int[100];
+    int count = 0;
+
+    void addItem(String itemName, double price, int qty)
+    {
+        itemNames[count] = itemName;
+        prices[count] = price;
+        quantities[count] = qty ;
+        count ++;
+    }
+
+    double getSubtotal() {
+        double subtotal = 0;
+
+        for (int i = 0; i< count ; i++)
+        {
+            subtotal += prices[i] * quantities[i];
+
+        }
+
+        return subtotal;
+    }
+
+    double applyDiscount(double percentage)
+    {
+        double subtotal = getSubtotal();
+        return subtotal - (subtotal * percentage / 100);
+    }
+
+    double calculateTotalWithTax(double taxRate , double discountPercentage)
+    {
+        double total = applyDiscount(discountPercentage);
+        total = total + (total * taxRate /100);
+        return total;
+    }
+}
+
+public class task2 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        ShoppingCart cart = new ShoppingCart();
+
+        while (true)
+        {
+            String item = input.next();
+
+            if(item.equalsIgnoreCase("Discount"))
+            {
+                break;
+            }
+
+            double price = input.nextDouble();
+
+            int qty = input.nextInt();
+
+            cart.addItem(item, price , qty);
+
+        }
+
+        double discount = input.nextDouble();
+
+        input.next();
+        double tax = input.nextDouble();
+
+        System.out.println("Subtotal : " + (int) cart.getSubtotal());
+        System.out.println("Total after " + (int) discount + "% discount and " +(int) tax + "% tax : " + cart.calculateTotalWithTax(tax, discount));
+
+        input.close();
+    }
+}
 
