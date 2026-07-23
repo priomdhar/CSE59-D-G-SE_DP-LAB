4c0bcf1aedb4eae4e9dda448f3b095369f1cd43e
priomdhar <priomdhar1010@gmail.com>
2026-07-23 16:03:12 +0600

    Implement Course class for student management

--- Task 8 (Lab exam 1 practice)/Main1.java
+++ Task 8 (Lab exam 1 practice)/Main1.java
@@ -1,1 +1,76 @@
+import java.util.Scanner;
 
+class Course {
+    String courseName;
+    String[] studentNames = new String[100];
+    double[] studentMarks = new double[100];
+
+    int count = 0;
+
+    Course(String courseName)
+    {
+        this.courseName = courseName;
+    }
+
+    void addStudent(String name,double marks)
+    {
+        studentNames[count] = name;
+        studentMarks[count] = marks;
+        count++;
+    }
+
+    String getHighestScorer() 
+    {
+        int highestIndex = 0;
+
+        for(int i =1 ; i < count; i++)
+        {
+            if(studentMarks[i] > studentMarks[highestIndex])
+            {
+                highestIndex = i;
+            }
+        }
+        return studentNames[highestIndex] + "(" + (int)studentMarks[highestIndex] + ")" ;
+
+    }
+    
+    int countTopGrades(double threshold)
+    {
+        int total = 0;
+
+        for(int i = 0 ; i<count ; i++)
+        {
+            if(studentMarks[i] >= threshold)
+            {
+                total ++;
+            }
+        }
+
+        return total;
+    }
+}
+
+public class task1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String courseName = input.next();
+        Course course = new Course(courseName);
+
+        while(input.hasNext()){
+            String name = input.next();
+            if(!input.hasNextDouble())
+            break;
+
+            double marks = input.nextDouble();
+            course.addStudent(name, marks);
+
+
+        }
+
+        System.out.println("Highest Scorer : "+ course.getHighestScorer());
+        System.out.println("Number pf A+ (80) : " + course.countTopGrades(80));
+        
+        input.close();
+    }
+}
